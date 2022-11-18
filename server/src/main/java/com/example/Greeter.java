a8d926355655c29b98c5fc62a2b5b6bd0e5cd65a
ulrich mbakop <ulrichmbakop@ulrichs-mbp.myfiosgateway.com>
2022-11-17 19:39:17 -0500

    testing scan

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -4,18 +4,16 @@ package com.example;
  * This is a class.
  */
 public class Greeter {
-
   /**
    * This is a constructor.
    */
   public Greeter() {
-
   }
-
  /**
    * This is a method.
    */
   public final String greet(final String someone) {
+    String DB_PASSWORD = "Admin@12345";
     return String.format("Hello Avinash, %s!", someone);
   }
 }
